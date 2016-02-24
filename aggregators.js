702d4749d410c21ab7c3dce1594747de448eeb8c
Roger Vandervort <rvandervort@gmail.com>
2016-02-24 10:29:08 -0500

    Adds aggregator for 'Average' calculation
    Fixes #1

--- aggregators.js
+++ aggregators.js
@@ -1,10 +1,39 @@
 var exports = module.exports = {
+
   'count': {
-      accumulator: function(currentCell, rowValue) { currentCell.value++; },
-      emitter: function(cell) { return cell.value; }
+      accumulator: function(currentCell, rowValue) {
+                      currentCell.value++;
+                   },
+
+      emitter: function(cell) {
+                 return cell.value;
+               }
    },
+
   'sum': {
-    accumulator: function(currentCell, rowValue) { currentCell.value += rowValue; },
-    emitter: function(cell) { return cell.value; }
+    accumulator: function(currentCell, rowValue) {
+                  currentCell.value += rowValue;
+                 },
+
+    emitter: function(cell) {
+                return cell.value;
+             }
+  },
+
+  'average': {
+    accumulator: function(currentCell, rowValue) {
+       if (typeof currentCell.sum == 'undefined')
+         currentCell.sum = 0;
+
+       currentCell.sum += rowValue;
+
+       if (typeof currentCell.count == 'undefined')
+         currentCell.count = 0;
+
+       currentCell.count++;
+    },
+    emitter: function(cell) {
+               return cell.sum / cell.count;
+             }
   }
 };
--- test/aggregators_test.js
+++ test/aggregators_test.js
@@ -27,4 +27,19 @@ describe('Aggregators', function() {
       expect(f.emitter(cell)).to.equal(1337);
     });
   });
+
+  describe('#average', function() {
+    it('emits the average of supplied values', function() {
+      var cell = {value: 0};
+      var rowValues = [1, 3, 5];
+
+      var f = aggregators['average'];
+
+      rowValues.forEach(function(value) {
+        f.accumulator(cell, value); 
+      });
+
+      expect(f.emitter(cell)).to.equal(3);
+    });
+  });
 });
